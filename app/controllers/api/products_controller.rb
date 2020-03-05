ca256ffa372225f69ffdd0546ca1ac6f50320950
Yossi <yosef.d.cohen@gmail.com>
2020-03-04 21:40:08 -0600

    add method to show any product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,10 @@
 class Api::ProductsController < ApplicationController
+  def query_all_products
+    # id = params[:id].to_i
+
+    @product = Product.find_by(id: 1)
+    p @product
+     
+    render 'query_all_products.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  
+  namespace :api do
+    get "/query_all_products" => "products#query_all_products"
+  end
 end
